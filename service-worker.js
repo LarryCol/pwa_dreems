333c67cd877f583296b5e5ab18db9c534ab972b1
LarryCol <kinglarrycollins@gmail.com>
2020-06-03 16:32:31 +0530

    Adding cache feature to service worker, for offline working.(refer mdn/pwq-examples in github) changed background for about us page

--- service-worker.js
+++ service-worker.js
@@ -2,10 +2,33 @@ console.log('Hello from service-worker.js');
 
 // To load Workbox from our CDN, update your service worker file to import the workbox-sw.js file via the importScripts() method.
 
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
+// importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
-if (workbox) {
-  console.log(`Yay! Workbox is loaded 🎉`);
-} else {
-  console.log(`Boo! Workbox didn't load 😬`);
-}
+// if (workbox) {
+//   console.log(`Yay! Workbox is loaded 🎉`);
+// } else {
+//   console.log(`Boo! Workbox didn't load 😬`);
+// }
+
+self.addEventListener('install', function(e) {
+ e.waitUntil(
+   caches.open('video-store').then(function(cache) {
+     return cache.addAll([
+       '/',
+       '/index.html',
+       '/about_us.html',
+       '/icons/dreem-192.png',
+       '/dreem-512.png'
+     ]);
+   })
+ );
+});
+
+self.addEventListener('fetch', function(e) {
+  console.log(e.request.url);
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
